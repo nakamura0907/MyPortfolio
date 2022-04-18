8493849b97e590323379730e77ba5e43d45c4ed0
ritsuki990907 <ritsuki990907@gmail.com>
2022-04-18 18:44:26 +0900

    スキルセットのコンポーネント分割

--- src/components/molecules/about/index.tsx
+++ src/components/molecules/about/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Avatar from '../../atoms/avatar'
-import { Progress } from '../../atoms/observer'
-import Profile from '../../atoms/profile'
+import Profile from '../profile'
+import Skills from '../skills'
 
 import './style.css'
 
@@ -20,18 +20,7 @@ const About = () => {
           <p></p>
         </div> */}
 
-        <div className="skills">
-          <h3 className="section-title-sub">スキルセット</h3>
-          <p>&#8251; もっとも得意なスキルを基準にしています</p>
-          <div className="skills-wrap">
-            <Progress title="PHP" value={100} />
-            <Progress title="JavaScript" value={80} />
-            <Progress title="Node.js" value={75} />
-            <Progress title="React" value={70} />
-            <Progress title="TypeScript" value={55} />
-            <Progress title="Flutter" value={20} />
-          </div>
-        </div>
+        <Skills />
       </div>
     </section>
   )
