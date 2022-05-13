bbb23f5d6c75b337e9dc6af3ffb4de3f6d086e36
ritsuki990907 <ritsuki990907@gmail.com>
2022-05-13 16:08:46 +0900

    HERO部分の修正

--- src/components/molecules/hero/index.tsx
+++ src/components/molecules/hero/index.tsx
@@ -10,6 +10,7 @@ interface Props {
 const Hero: React.FC<Props> = ({ handleClick }) => (
   <article className="hero">
     <div className="hero-background" style={{ backgroundImage: `url(${Avatar})` }}></div>
+    <img className="hero-image" src={Avatar} alt="背景" />
     <h1 className="hero-title">ようこそ</h1>
     <div className="hero-arrow" onClick={handleClick}>
       &darr;
